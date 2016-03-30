db43866cb4d07ad5dd07d01e9fa7b19d8c8257f6
vincseize <vincseize@gmail.com>
2016-03-30 13:20:09 +0200

    Update karlovaboardzatorMain.py

--- karlovaboardzatorMain.py
+++ karlovaboardzatorMain.py
@@ -33,24 +33,13 @@ class __QT_KBZ__(QtGui.QDialog):
 		# self.printSTD(self.CURENT_PROJECT)
 
 	#======================================================================
-	#========= Globals Colors
-	#======================================================================
-
-		# green_palette.setColor(QtGui.QPalette.WindowText,QtGui.QColor.fromHsv(120, 255, 255))
-
-		self.palette_grey = QtGui.QPalette()
-		self.palette_grey.setColor(QtGui.QPalette.Background,QtCore.Qt.gray)
-
-		self.palette_darkGrey = QtGui.QPalette()
-		self.palette_darkGrey.setColor(QtGui.QPalette.Background, QtGui.QColor.fromHsv(66, 66, 66))
-
-	#======================================================================
 	#========= main vlayout
 	#======================================================================
 		self.mainLayout = QtGui.QVBoxLayout()
 
 		#========= Top Bandeau container
 		self.TopAreaContainer = QtGui.QWidget()
+
 		#========= Top Bandeau content
 		self.TopAreaContent = QtGui.QHBoxLayout()
 
@@ -79,9 +68,6 @@ class __QT_KBZ__(QtGui.QDialog):
 		self.tabArea.setTabPosition(QtGui.QTabWidget.North)
 		# self.tabArea.setTabPosition(QtGui.QTabWidget.West)
 		# self.setWidget(self.tabArea)
-
-
-
 
 
 		# #=========================== Tab1
@@ -135,11 +121,9 @@ class __QT_KBZ__(QtGui.QDialog):
 	#======================================================================
 		self.centralWidget = QtGui.QWidget()
 		self.setLayout(self.mainLayout)
+		#========= apply stylsheets
+		self.apply_Stylesheets()
 		self.setPalette(self.palette_darkGrey)
-
-
-
-
 
 	#======================================================================
 	#========= Functions
@@ -163,10 +147,97 @@ class __QT_KBZ__(QtGui.QDialog):
 
 
 
+	#======================================================================
+	#========= StyleSheets
+	#======================================================================
+
+	def apply_Stylesheets(self):
+
+		def rvbToHex(r,g,b):
+			# r = array_rgb[1], g=array_rgb[2], b=array_rgb[3]
+			hexColor = '#%02x%02x%02x' % (r, g, b)
+			return hexColor
+
+
+
+	#======================================================= Globals Colors
+
+	#========= general Colors	
+
+		self.palette_White = QtGui.QPalette()
+		self.palette_White.setColor(QtGui.QPalette.Background, QtGui.QColor.fromHsv(255, 255, 255))
+
+		self.palette_Black = QtGui.QPalette()
+		self.palette_Black.setColor(QtGui.QPalette.Background, QtGui.QColor.fromHsv(0, 0, 0))
+
+		self.palette_grey = QtGui.QPalette()
+		self.palette_grey.setColor(QtGui.QPalette.Background,QtCore.Qt.gray)
+
+		self.palette_mediumGrey = QtGui.QPalette()
+		self.palette_mediumGrey.setColor(QtGui.QPalette.Background, QtGui.QColor.fromHsv(128, 128, 128))
+
+		self.palette_darkGrey = QtGui.QPalette()
+		self.palette_darkGrey.setColor(QtGui.QPalette.Background, QtGui.QColor.fromHsv(66, 66, 66))
+
+	#============================================ Complementary blues Colors # http://paletton.com	
+
+	#========= blues Colors
+
+		self.palette_Blue1 = QtGui.QPalette()
+		self.palette_Blue1.setColor(QtGui.QPalette.Background, QtGui.QColor.fromHsv(97, 114, 141))
+
+		self.palette_Blue2 = QtGui.QPalette()
+		self.palette_Blue2.setColor(QtGui.QPalette.Background, QtGui.QColor.fromHsv(64, 84, 115))
+
+		self.palette_Blue3 = QtGui.QPalette()
+		self.palette_Blue3.setColor(QtGui.QPalette.Background, QtGui.QColor.fromHsv(43, 61, 91))
+
+		self.palette_Blue4 = QtGui.QPalette()
+		self.palette_Blue4.setColor(QtGui.QPalette.Background, QtGui.QColor.fromHsv(25, 44, 75))
+
+		self.palette_Blue5 = QtGui.QPalette()
+		self.palette_Blue5.setColor(QtGui.QPalette.Background, QtGui.QColor.fromHsv(10, 25, 50))
+
+	#========= blues Colors
+
+		self.palette_orange1 = QtGui.QPalette()
+		self.palette_orange1.setColor(QtGui.QPalette.Background, QtGui.QColor.fromHsv(255, 196, 77))
+
+		self.palette_orange2 = QtGui.QPalette()
+		self.palette_orange2.setColor(QtGui.QPalette.Background, QtGui.QColor.fromHsv(255, 179, 27))
+
+		self.palette_orange3 = QtGui.QPalette()
+		self.palette_orange3.setColor(QtGui.QPalette.Background, QtGui.QColor.fromHsv(255, 170, 0))
+
+		self.palette_orange4 = QtGui.QPalette()
+		self.palette_orange4.setColor(QtGui.QPalette.Background, QtGui.QColor.fromHsv(206, 137, 0))
+
+		self.palette_orange5 = QtGui.QPalette()
+		self.palette_orange5.setColor(QtGui.QPalette.Background, QtGui.QColor.fromHsv(160, 107, 0))
+
+
+	#========= Style Buttons
+
+		hexColor = rvbToHex(25, 44, 75)
+		self.BT_MAIN_1.setStyleSheet('QPushButton {background-color: '+hexColor+'; color: white;}')
+		self.BT_MAIN_2.setStyleSheet('QPushButton {background-color: '+hexColor+'; color: white;}')
+		self.BT_MAIN_3.setStyleSheet('QPushButton {background-color: '+hexColor+'; color: white;}')
+
+
+
+#===================================================================================================================================
+#========= Start QT 
+#===================================================================================================================================
+
+
 def start(parent, data):
 	print >> sys.__stderr__, "__QT_KBZ__"
 	main = __QT_KBZ__(parent)
-	main.setWindowTitle('KARLOVA DASHBOARDZATOR | Welcome Dear ' + os.getenv('USER'))
+	array_welcome = ['Welcome Dear', 'Welcome', 'Willkommen', 'Welkom']
+	array_welcome = array_welcome + ['Bienvenue', 'Bienvenue Fulgence']
+	array_welcome = array_welcome + ['Xos', 'Ongietorri', 'I mirepritur']
+	welcome = random.choice(array_welcome)
+	main.setWindowTitle( ink.io.ConnectUserInfo()[2].upper() + ' | KARLOVA DASHBOARDZATOR | '+ welcome +' ' + os.getenv('USER') )
 	main.showMaximized()
 	# sG = QtGui.QApplication.desktop().screenGeometry()
 	# w = sG.width
