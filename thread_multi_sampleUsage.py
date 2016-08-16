060ac9a6f72b94eac51576a074cf1e59b6dd9cae
vincseize <vincseize@gmail.com>
2016-08-16 17:32:11 +0200

    Update thread_multi_sampleUsage.py

--- thread_multi_sampleUsage.py
+++ thread_multi_sampleUsage.py
@@ -4,82 +4,95 @@
 # MG ILLUMINATION                                                                  #
 # Author : cPOTTIER                                                                #
 # Date : 16-08-2016                                                                #
-# Thread Sample Usage                                                              #
 # ##################################################################################
 
-import sys, os 
+import os, sys, random
+import glob
+import time, datetime
+from datetime import datetime
+import json
+import shutil
 
-if '__Thread__class' in sys.modules:
-    del(sys.modules["__Thread__class"])
-    import __Thread__class
-    from __Thread__class import * 
+from PyQt4 import QtGui, QtCore, Qt
+from PyQt4.QtCore import QThread
+
+import threading
+from threading import *
+import Queue
+from Queue import Queue
+from multiprocessing import Pool, Process, Pipe, Lock, Value, Array, Manager, TimeoutError
+
+#========================================================================================================================== 
+
+if '__FUNCTIONS__TOTHREAD__' in sys.modules:
+    del(sys.modules["__FUNCTIONS__TOTHREAD__"])
+    import thread_multi_sampleUsage
+    from thread_multi_sampleUsage import *
+    # from thread_multi_sampleUsage import __FUNCTIONS__TOTHREAD__
 else:
-    import __Thread__class
-    from __Thread__class import *
+    import thread_multi_sampleUsage
+    from thread_multi_sampleUsage import *
+    # from thread_multi_sampleUsage import __FUNCTIONS__TOTHREAD__
     
-#===================================================================================================
+locked = RLock()
+# workQueue = Queue.Queue(10)
 
-class __FUNCTIONS__TOTHREAD__():
-    '''Dont Touch'''  
-    def __init__(self, args, with_locked=True):
-        self.args = args
-        self.with_locked = with_locked # True force process ending before to execute the next
-        self.run()
+#============================================================================================= Thread Instance ( container )
 
-    def run(self):
-	'''Dont Touch'''
-	# print self.args 
-	if self.with_locked == True:
-	    with locked: 
-		self.your_function()
-	else:
-	    self.your_function()
-		
-#=================================================================================================== 
+class __THREAD__INSTANCE__():
 
-    def your_function(self):
-	'''Your Code Here'''
-	import random
-	from random import randint
-	for arg in self.args:
-	    for lettre in arg:
-		print lettre
-		delay = 0.05
-		delay += random.randint(1, 60) / 100
-		time.sleep(delay)
+	def __init__(self, *args): 
+		self.type_process = 'stack'
+		self.with_locked  = False
+		try:
+		    self.type_process = args[1]
+		except:
+		  pass
+		try:
+		    self.with_locked = args[2]
+		except:
+		  pass
+		self.threads  	= []
+		arguments     	= []
+		for arg in args[0]:
+			arguments.append(arg)
+		t = __THREAD__WORKER__(arguments, self.with_locked, self) # self very Important
+		t.start()
+		self.threads.append(t)
+		if self.type_process == 'stack':
+			for t in self.threads:
+				t.join()
 
-		
-# ##################################################################################
-# METHOD                                                                           #                                                               #                                                            #
-# ##################################################################################
+	def __del__(self):
+		for t in self.threads:
+			running 	= t.running()
+			t.stop()
+			if not t.finished():
+				t.wait()
 
-#============================= type of sharing ressources process | optional ( default stack ) 
-# type_process = 'stack' 	# thread are executed in order
-type_process = 'parallel'	# thread are executed simultaneous
 
-#=========================================== lock process | optional ( default True (faster) )
-with_locked = False		# lock and wait end of run threading process function
+#======================================================================================================= Thread ( Worker )
 
-#============================================================== var(s) for threading process 1
-film0 = 'grinch'
-film1 = 'pets'
-film2 = 'lorax'
-film3 = 'dm1'
-film4 = 'dm2'
-film5 = 'dm3'
-film6 = 'pets'
-args = [film0,film1,film2,film3,film4,film5,film6]
+class __THREAD__WORKER__(threading.Thread): # If QT, no threading.Thread
 
-#============================================================== var(s) for threading process 2
-xvar0 = 'LORAX'
-xvar1 = 'DM3'
-xvar2 = 'LASOUPEAUXCHOUX'
-xargs = [xvar0,xvar1,xvar2]
+	def __init__(self,args ,with_locked, receiver):
+	  
+		# QtCore.QThread.__init__(self) # if in QT
+		threading.Thread.__init__(self)
+		self.args     		= args
+		self.with_locked	= with_locked
+		self.receiver   	= receiver # receiver ( self ) very Important
 
-#=========== method to run several functions =================================================
+		self.Terminated 	= 0
 
-__Thread__class.__THREAD__INSTANCE__(args,type_process,with_locked) 	
-__Thread__class.__THREAD__INSTANCE__(xargs,type_process,with_locked)
+	def run(self):
+		try:
+			FUNCTIONS__TOTHREAD = thread_multi_sampleUsage.__FUNCTIONS__TOTHREAD__(self.args,self.with_locked)
+		except AttributeError:
+		    print AttributeError
+		    return None
 
+	def stop(self):
+		self.Terminated 	= 1
 
 
