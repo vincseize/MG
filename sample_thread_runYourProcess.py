ecf4f8173a5671d8341c895d353e5406edaa6849
vincseize <vincseize@gmail.com>
2016-08-16 16:21:21 +0200

    Update sample_thread_runYourProcess.py

--- sample_thread_runYourProcess.py
+++ sample_thread_runYourProcess.py
@@ -19,11 +19,14 @@ else:
     from __Thread__class import *
 #=============================================================================================
 
-#========== type of threading process , to share ressources process
-# type_process = 'stack' 	# thread are executed in order 
+#========================================================== type of sharing ressources process
+# type_process = 'stack' 	# thread are executed in order
 type_process = 'parallel'	# thread are executed simultaneous
 
-#========== var(s) for threading process 1
+#==================================================== lock process | optional ( default True )
+with_locked = False		# lock and wait end of run threading process function
+
+#============================================================== var(s) for threading process 1
 film0 = 'grinch'
 film1 = 'pets'
 film2 = 'lorax'
@@ -31,21 +34,16 @@ film3 = 'dm1'
 film4 = 'dm2'
 film5 = 'dm3'
 film6 = 'pets'
-
 args = [film0,film1,film2,film3,film4,film5,film6]
 
-#========== don t touch =====================================================================
-
-__Thread__class.__THREAD__INSTANCE__(args,type_process) 	# type_process optional
-
-#========== var(s) for threading process 2
+#============================================================== var(s) for threading process 2
 xvar0 = 'LORAX'
 xvar1 = 'DM3'
 xvar2 = 'LASOUPEAUXCHOUX'
 xargs = [xvar0,xvar1,xvar2]
 
-#========== don t touch =====================================================================
+#========== methode to run several functions =================================================
 
-__Thread__class.__THREAD__INSTANCE__(args,type_process) 	# type_process optional
-__Thread__class.__THREAD__INSTANCE__(xargs,type_process)	# type_process optional
+__Thread__class.__THREAD__INSTANCE__(args,type_process,with_locked) 	
+__Thread__class.__THREAD__INSTANCE__(xargs,type_process,with_locked)
 
