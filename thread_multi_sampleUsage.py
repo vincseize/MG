cbc20d0af99e92f8bfedb5d02f6876788db2d9e1
vincseize <vincseize@gmail.com>
2016-08-16 17:31:43 +0200

    Update thread_multi_sampleUsage.py

--- thread_multi_sampleUsage.py
+++ thread_multi_sampleUsage.py
@@ -7,11 +7,7 @@
 # Thread Sample Usage                                                              #
 # ##################################################################################
 
-
-#======================================================================= threads CLASSES dont touch
-import sys, os, time 
-import random
-from random import randint
+import sys, os 
 
 if '__Thread__class' in sys.modules:
     del(sys.modules["__Thread__class"])
@@ -21,45 +17,69 @@ else:
     import __Thread__class
     from __Thread__class import *
     
-# locked = RLock()
-# workQueue = Queue.Queue(10)
 #===================================================================================================
 
-
 class __FUNCTIONS__TOTHREAD__():
-  
+    '''Dont Touch'''  
     def __init__(self, args, with_locked=True):
         self.args = args
-        self.with_locked = with_locked # True forced process ending before to execute the next
+        self.with_locked = with_locked # True force process ending before to execute the next
         self.run()
 
     def run(self):
 	'''Dont Touch'''
-	
-	# print self.args # for debug
-	
-        i = 0
-        while i < 2:
-	  if self.with_locked == True:
+	# print self.args 
+	if self.with_locked == True:
 	    with locked: 
 		self.your_function()
-	    i += 1
-
-	  else:
+	else:
 	    self.your_function()
-	    i += 1
-	    
-	  
+		
+#=================================================================================================== 
 
     def your_function(self):
-	'''Your Code'''
+	'''Your Code Here'''
+	import random
+	from random import randint
 	for arg in self.args:
-		for lettre in arg:
-		    print lettre
-		    delay = 0.05
-		    delay += random.randint(1, 60) / 100
-		    time.sleep(delay)
+	    for lettre in arg:
+		print lettre
+		delay = 0.05
+		delay += random.randint(1, 60) / 100
+		time.sleep(delay)
+
 		
+# ##################################################################################
+# METHOD                                                                           #                                                               #                                                            #
+# ##################################################################################
+
+#============================= type of sharing ressources process | optional ( default stack ) 
+# type_process = 'stack' 	# thread are executed in order
+type_process = 'parallel'	# thread are executed simultaneous
+
+#=========================================== lock process | optional ( default True (faster) )
+with_locked = False		# lock and wait end of run threading process function
+
+#============================================================== var(s) for threading process 1
+film0 = 'grinch'
+film1 = 'pets'
+film2 = 'lorax'
+film3 = 'dm1'
+film4 = 'dm2'
+film5 = 'dm3'
+film6 = 'pets'
+args = [film0,film1,film2,film3,film4,film5,film6]
+
+#============================================================== var(s) for threading process 2
+xvar0 = 'LORAX'
+xvar1 = 'DM3'
+xvar2 = 'LASOUPEAUXCHOUX'
+xargs = [xvar0,xvar1,xvar2]
+
+#=========== method to run several functions =================================================
+
+__Thread__class.__THREAD__INSTANCE__(args,type_process,with_locked) 	
+__Thread__class.__THREAD__INSTANCE__(xargs,type_process,with_locked)
 
 
 
